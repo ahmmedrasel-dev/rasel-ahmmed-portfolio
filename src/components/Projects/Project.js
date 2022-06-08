02fabfd645dd2f803f29191ed9eec793b1645c92
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-06-09 02:03:56 +0300

    update project page

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -10,20 +10,23 @@ const Project = ({ project }) => {
   }
 
   return (
-    <div className="flex items-center shadow-xl p-4">
-      <figure><img className='w-96 rounded-lg' src={project_thubnail} alt="Album" /></figure>
+    <div className="shadow-xl p-4">
+      <h2 className="text-2xl uppercase py-4">{project_name}</h2>
+
+      <figure><img className='w-full rounded-lg' src={project_thubnail} alt="Album" /></figure>
       <div className="ml-4">
-        <h2 className="text-xl">{project_name}</h2>
+
         <ul className='my-4'>
+          <li>Main Features:</li>
           {
-            projects_feature.map(feature => <li className='text-sm'>{feature}</li>)
+            projects_feature.map(feature => <li className='text-sm pl-4'>{feature}</li>)
           }
         </ul>
         <div className="flex lg:flex-row flex-col justify-between">
-
-          <div className='flex'>
+          <div className='flex gap-4'>
             <button className="btn btn-warning btn-sm" onClick={() => navigateToPurchase(_id)}>Code Link</button>
             <button className='btn btn-sm btn-success'>Live Link</button>
+            <button className='btn btn-sm btn-success'>Server Code Link</button>
           </div>
         </div>
 
