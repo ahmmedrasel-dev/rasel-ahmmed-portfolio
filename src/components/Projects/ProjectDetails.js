3dec3b3e300f6b85e59088a2c6396a02323b5726
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-06-10 19:11:51 +0300

    project details

--- src/components/Projects/ProjectDetails.js
+++ src/components/Projects/ProjectDetails.js
@@ -1,72 +1,66 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import { AiFillCheckSquare } from "react-icons/ai";
 
 const ProjectDetails = () => {
   const { id } = useParams()
-  const [project, setProject] = useState({})
 
-  const { project_name,
-    img_1,
-    img_2,
-    img_3,
-    projects_feature,
-    live_link,
-    client_site_link,
-    server_site_link,
-    project_description,
-    technology } = project
-
-
-  useEffect(() => {
-    const url = `http://localhost:5000/project/${id}`;
-    const data = async () => {
-      const { data } = await axios.get(url);
-      setProject(data)
+  const { data: project, isLoading } = useQuery('project',
+    async () => {
+      try {
+        const { data } = await axios.get(`https://rasel-web-portfolio.herokuapp.com/project/${id}`);
+        // console.log(data);
+        return data;
+      }
+      catch (error) {
+        console.log(error.message)
+      }
     }
-    data()
+  )
 
-  }, [id])
-
+  if (isLoading) {
+    return 'Loadign ....'
+  }
 
 
   return (
     < div className='bg-neutral py-8' >
       <div className='lg:max-w-7xl w-full lg:mx-auto mx-4'>
-        <h1 className='text-5xl text-center py-4'>{project_name}</h1>
-        <img className='text-center' src={img_1} alt="" />
+        <h1 className='text-5xl text-center py-8'>{project.project_name}</h1>
+        <img className='text-center' src={project.img_1} alt="" />
 
-        <p className='text-xl py-4'>{project_description}</p>
+        <p className='text-xl py-4'>{project.project_description}</p>
 
-        <img src={img_2} alt="" />
+        <img src={project.img_2} alt="" />
         <p className='text-2xl py-4'>Main Features: </p>
-        <ul>
+        <ul className='mb-8'>
           {
-            projects_feature.map((feature, index) => <li
-              className='text-lg py-2'
+            project.projects_feature.map((feature, index) => <li
               key={index}
-            >
-              {feature}
-            </li>)
+              className="flex items-center"
+            ><AiFillCheckSquare className='mr-3' /> {feature}</li>)
           }
         </ul>
-        <img src={img_3} alt="" />
+
+        <img src={project.img_3} alt="" />
+
+        <p className='text-2xl py-4'>Technology Used: </p>
         {
-          technology.map((tech, index) => <button
-            key={index}
-            className="btn btn-outline m-2 btn-sm btn-warning"
-          >#{tech}</button>
-          )
+          project.technology.map(tech => <button
+            class="btn btn-outline btn-warning m-2"
+
+          >{tech}</button>)
         }
-
-
-        <div className='flex gap-5'>
-          <a className='btn btn-xl btn-success' href={live_link}>Live Link</a>
-          <a className='btn btn-xl btn-success' href={client_site_link}>Client Site Link</a>
+        <div className='flex gap-5 mt-8'>
+          <a className='btn btn-xl btn-success' href={project.live_link} target="_blank" rel="noreferrer">Live Link</a>
+          <a className='btn btn-xl btn-success' href={project.client_site_link} target="_blank" rel="noreferrer">Client Site Code Link</a>
 
           {
-            server_site_link && <a className='btn btn-xl btn-success' href={server_site_link}>Server Site Link</a>
+            project.server_site_link && <a className='btn btn-xl btn-success' href={project.server_site_link}>Server Site Code Link</a>
           }
+
         </div>
       </div>
     </div >
